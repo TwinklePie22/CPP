b77fb05f41465d6aafd870f53feecace6633d93e
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-07-10 10:35:45 +0530

    Update inline.cpp

--- inline.cpp
+++ inline.cpp
@@ -9,7 +9,6 @@ int main()
     int num1, num2, num3;
     cout << "Enter three numbers: \n";
     cin >> num1 >> num2 >> num3;
-    int largest = findLargest(num1, num2, num3);
-    cout << "The largest number is: " << largest << endl;
+    cout << "The largest number is: " << findLargest(num1, num2, num3)<<endl;
     return 0;
 }
