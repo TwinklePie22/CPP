b45c55700fb7c76f8828faa984dff14805e4dc35
TWINKLE PIE <119886591+TwinklePie22@users.noreply.github.com>
2023-07-24 03:31:57 +0530

    Update accounts.cpp

--- accounts.cpp
+++ accounts.cpp
@@ -3,52 +3,54 @@
 using namespace std;
 struct bank
 {
-    char name[20];
+    string name;
     double bal;
     long acc;
 };
 int main()
 {
-    int n, i;
-    cout << "Enter no. of entries : ";
+    int n;
+    cout << "Enter no. of entries: ";
     cin >> n;
     struct bank arr[n];
     ofstream outfile("F:\\bank.txt");
     if (!outfile)
     {
-        cout << "Cannot outfileen file \n";
+        cout << "Cannot open file\n";
         return 1;
     }
-    for (i = 0; i < n; i++)
+    outfile << "Name,Account Number,Balance\n";
+    for (int i = 0; i < n; i++)
     {
         cout << "\nEnter name of customer " << i + 1 << ": ";
-        cin >>arr[i].name;
+        cin >> arr[i].name;
         cout << "Enter a/c number of customer " << i + 1 << ": ";
-        cin >> arr[i].acc ;
-        cout << "Enter balamce of customer " << i + 1 << ": ";
-        cin >>arr[i].bal;
-        outfile.write((char *)&arr[i], sizeof(struct bank));
+        cin >> arr[i].acc;
+        cout << "Enter balance of customer " << i + 1 << ": ";
+        cin >> arr[i].bal;
+        outfile << arr[i].name << "," << arr[i].acc << "," << arr[i].bal << "\n";
     }
     outfile.close();
-    
     ifstream infile("F:\\bank.txt");
     if (!infile)
     {
-        cout << "File doesn't exists \n";
+        cout << "File doesn't exist\n";
         return 1;
     }
-    for (i = 0; i < n; i++)
+    string header;
+    getline(infile, header); // Skip the header line
+    for (int i = 0; i < n; i++)
     {
-        if (infile)
-        {   
-            cout<<"\nDetails of customer "<<i+1;
-            infile.read((char *)&arr[i], sizeof(struct bank));
-            cout << "Name : " << arr[i].name << "\n";
-            cout << "A/c number  : " << arr[i].acc << "\n";
-            cout << "Balance (in INR) : " << arr[i].bal << "\n";
-        }
+        cout << "\nDetails of customer " << i + 1 << endl;
+        getline(infile, arr[i].name, ',');
+        infile >> arr[i].acc;
+        infile.ignore(); // Ignore the comma
+        infile >> arr[i].bal;
+        infile.ignore(); // Ignore the newline character
+        cout << "Name: " << arr[i].name << "\n";
+        cout << "A/c number: " << arr[i].acc << "\n";
+        cout << "Balance (in INR): " << arr[i].bal << "\n";
     }
-
     infile.close();
     return 0;
 }
